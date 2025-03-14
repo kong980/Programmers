84830836787ae1759ac726c6d6a89b7d77c39aa3
Sarah Kim <94705593+kong980@users.noreply.github.com>
2025-03-14 23:10:41 +0900

    [level 0] Title: 모스부호 (1), Time: 0.25 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String solution(String letter) {
+        String[] morse = { 
+            ".-","-...","-.-.","-..",".","..-.",
+            "--.","....","..",".---","-.-",".-..",
+            "--","-.","---",".--.","--.-",".-.",
+            "...","-","..-","...-",".--","-..-",
+            "-.--","--.."
+        };
+        
+        String[] morseString = letter.split(" ");
+        
+        StringBuilder sb = new StringBuilder();
+        for(String word : morseString){
+            for(int i=0; i<morse.length; i++){
+                if(word.equals(morse[i])){
+                    sb.append(Character.toString(i + 'a'));
+                    break;
+                }
+            }
+        }
+        
+        String answer = sb.toString();
+        return answer;
+    }
+}
