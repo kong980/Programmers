9062c5175980ba65e362b2115f763923fe4b78cb
Sarah Kim <94705593+kong980@users.noreply.github.com>
2025-03-19 15:59:20 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.14 ms, Memory: 82.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int num) {
+        int answer = 0;
+
+        while(num > 1 && answer < 500){
+            if(num % 2 == 0){
+                num /= 2;
+                answer++;
+            }else{
+                num = (num * 3) + 1;
+                answer++;
+                if(num < 0) {
+                    answer = -1;
+                }
+            }
+        }
+        return answer;
+    }
+}
