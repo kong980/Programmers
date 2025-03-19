4b55b6968e26623f297d6e94832061c3f269a523
Sarah Kim <94705593+kong980@users.noreply.github.com>
2025-03-19 17:11:34 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 45;
+
+        
+        for(int i : numbers){
+           answer -= i;
+        }
+
+        
+        
+        return answer;
+    }
+}
