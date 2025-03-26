229cf4848786a8e67b1824981c06aa98b854baf7
Sarah Kim <94705593+kong980@users.noreply.github.com>
2025-03-26 21:37:25 +0900

    Update 2의 영역.java

--- 프로그래머스/0/181894. 2의 영역/2의 영역.java
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.java
@@ -5,8 +5,7 @@ class Solution {
 
         // 1. min, max로 2가 나오는 첫번째, 마지막 index 찾기
         // 2. min = arr.length인 경우 -1 리턴
-        // 3. Arrays.copyOfRange() 이용
-        
+
         int min = arr.length; // 첫 번째 2
         int max = 0; // 마지막 2
         
